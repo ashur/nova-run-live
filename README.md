01d18f3b4d8e8d067b9fd750c79d36ff3b234228
Ashur Cabrera <ashur@panic.com>
2019-10-01 15:49:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Run Live
+
+Select a snippet of code and run it in the Nova extension runtime.
+
+## Usage
+
+Paste the following into a new JavaScript document:
+
+```javascript
+console.log( "hello, world" );
+```
+
+Select the text and press `Command-Shift-.`, or press `Command-Shift-,` to run the entire document:
+
+```
+Run Live[15:49:19.987000] hello, world
+```
+
+## Notes
+
+Selections which call `require` will fail
---
+++ scripts/main.js
@@ -0,0 +1,49 @@
+let tmp = nova.path.join( nova.extension.path, "tmp" );
+
+exports.activate = () =>
+{
+	if( nova.fs.listdir( tmp ).length > 0)
+	{		
+		nova.fs.rmdir( tmp );
+		nova.fs.mkdir( tmp );
+	}
+};
+
+nova.commands.register( "cab.ashur.runlive.run-editor", textEditor =>
+{
+	let range = new Range( 0, textEditor.document.length )
+	let contents = textEditor.document.getTextInRange( range );
+
+	runText( contents );
+});
+
+nova.commands.register( "cab.ashur.runlive.run-selection", textEditor =>
+{
+	runText( textEditor.selectedText );
+});
+
+function runText( text )
+{
+	if( text )
+	{
+		let basename = `${Date.now()}.js`;
+
+		let path = nova.path.join( tmp, basename );
+
+		let file = nova.fs.open( path, "w" );
+		file.write( text );
+
+		try
+		{
+			require( `../tmp/${basename}` );
+		}
+		catch( error )
+		{
+			console.log( error )
+		}
+		finally
+		{
+			nova.fs.remove( path );
+		}
+	}
+}
